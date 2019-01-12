e7e483d3ff894e268f22f8019e570fd564627181
timebird7 <timebird7@gmail.com>
2019-01-12 23:30:32 +0900

    190112

--- Baekjoon/2292x.py
+++ Baekjoon/2292x.py
@@ -1,20 +1,29 @@
-n = int(input())
+# n = int(input())
 
 
-ans = [1]
+# ans = [1]
 
-if n == 2:
-    print(2)
-else:
+# if n == 2:
+#     print(2)
+# else:
 
-    for x in range(2,n):
-        for t in range(0, 6*(x-1)):
-            ans.append(x)
-            if len(ans) > n-1:
-                break
-        else:
-            continue
-        break
+#     for x in range(2,n):
+#         for t in range(0, 6*(x-1)):
+#             ans.append(x)
+#             if len(ans) > n-1:
+#                 break
+#         else:
+#             continue
+#         break
 
 
-    print(ans[-1])
+#     print(ans[-1])
+# n= int(input())
+# ans = '1'
+# for x in range(2,int(n/100000)):
+#     ans += f'{x}'*6*(x-1)
+#     if len(ans) > n:
+#         print(x)
+#         break
+
+
