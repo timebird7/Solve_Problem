40e82ea9dea16dc005327fc9dffab1d6e4edbf27
timebird7 <timebird7@gmail.com>
2019-02-14 19:46:51 +0900

    190214

--- SWEA/1267.py
+++ SWEA/1267.py
@@ -2,47 +2,35 @@ class Stack:
     def __init__(self):
         self.stack = [0]*V
         self.pnt = 0
- 
+   
     def push(self, x):
         self.stack[self.pnt] = x
         self.pnt += 1
- 
-    def pop(self):
-        if self.pnt == 0:
-            return None
-        else:
-            self.pnt -= 1
-            return self.stack[self.pnt]
-     
-    def isEmpty(self):
-        return self.pnt == 0
- 
-    def peek(self):
-        if self.pnt == 0:
-            return None
-        else:
-            return self.stack[self.pnt-1]
- 
+   
+    def pop(self):        
+        self.pnt -= 1
+        return self.stack[self.pnt]
+   
+    def peek(self):        
+        return self.stack[self.pnt-1]
+   
 for tc in range(10):
     V, E = list(map(int,input().split())) 
     w = list(map(int,input().split()))
-    S = w[-1]
-    ways = [0 for i in range(E)]
-    for i in range(E):
-        a = [w[i*2+1], w[i*2]]
-        ways[i] = a
- 
+    S = w[-1]    
+   
     stack = Stack()
     ans = Stack()
     visited = [0 for i in range(V+1)]
- 
+   
     visited[S] = 1
     stack.push(S)
- 
+   
     while True:        
-        for way in ways:
-            if way[0] == S and visited[way[1]] == 0:                
-                S = way[1]
+        for i in range(E):
+            a, b = w[i*2+1], w[i*2]
+            if a == S and visited[b] == 0:                
+                S = b
                 stack.push(S)
                 visited[S] = 1
                 break
@@ -50,19 +38,19 @@ for tc in range(10):
             visited[S] = 1
             ans.push(stack.pop())
             S = stack.peek()
-         
-        if stack.isEmpty():
+           
+        if stack.pnt == 0:
             for i in range(1,V+1):
                 if visited[i] == 0:
                     S = i
             else:
                 stack.push(S)
- 
+   
         if ans.pnt == V:
             break
-         
-    result = ''
+           
+    r = ''
     for c in ans.stack:
-        result += ' ' + str(c)    
- 
-    print(f'#{tc+1}{result}')
+        r += ' ' + str(c)    
+   
+    print(f'#{tc+1}{r}')
