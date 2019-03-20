029e7b3bb9fae732d0f63eff265e2b6e1a693b8a
timebird7 <timebird7@gmail.com>
2019-03-20 15:05:33 +0900

    190320

--- SWEA/1240.py
+++ SWEA/1240.py
@@ -1,34 +1,34 @@
 def convert(pwd):
     result = [0,0,0,0,0,0,0,0]
     for i in range(8):
-        if pwd[7*i:7*i+7] == [0,0,0,1,1,0,1]:
+        if pwd[7*i:7*i+7] == '0001101':
             result[i] = 0
             continue
-        if pwd[7*i:7*i+7] == [0,0,1,1,0,0,1]:
+        if pwd[7*i:7*i+7] == '0011001':
             result[i] = 1
             continue
-        if pwd[7*i:7*i+7] == [0,0,1,0,0,1,1]:
+        if pwd[7*i:7*i+7] == '0010011':
             result[i] = 2
             continue
-        if pwd[7*i:7*i+7] == [0,1,1,1,1,0,1]:
+        if pwd[7*i:7*i+7] == '0111101':
             result[i] = 3
             continue
-        if pwd[7*i:7*i+7] == [0,1,0,0,0,1,1]:
+        if pwd[7*i:7*i+7] == '0100011':
             result[i] = 4
             continue
-        if pwd[7*i:7*i+7] == [0,1,1,0,0,0,1]:
+        if pwd[7*i:7*i+7] == '0110001':
             result[i] = 5
             continue
-        if pwd[7*i:7*i+7] == [0,1,0,1,1,1,1]:
+        if pwd[7*i:7*i+7] == '0101111':
             result[i] = 6
             continue
-        if pwd[7*i:7*i+7] == [0,1,1,1,0,1,1]:
+        if pwd[7*i:7*i+7] == '0111011':
             result[i] = 7
             continue
-        if pwd[7*i:7*i+7] == [0,1,1,0,1,1,1]:
+        if pwd[7*i:7*i+7] == '0110111':
             result[i] = 8
             continue
-        if pwd[7*i:7*i+7] == [0,0,0,1,0,1,1]:
+        if pwd[7*i:7*i+7] == '0001011':
             result[i] = 9
             continue
     
@@ -38,16 +38,16 @@ TC = int(input())
 
 for tc in range(TC):
     N, M = list(map(int,input().split()))
-    nums = [[] for i in range(N)]
-    pwd = []
+    nums = ['' for i in range(N)]
+    pwd = ''
 
     for n in range(N):
-        nums[n] = list(map(int,list(input())))
+        nums[n] = input()
 
     for n in range(n):
-        if sum(nums[n]):
+        if int(nums[n]):
             for i in range(M-1,-1,-1):
-                if nums[n][i]:
+                if int(nums[n][i]):
                     pwd = nums[n][i-55:i+1]
                     break
 
